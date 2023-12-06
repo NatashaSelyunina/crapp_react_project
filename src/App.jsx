a3213c070069a6a260e602c65f570c976329dfc8
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2023-12-06 11:07:19 +0300

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+const Profile = () => {
+  return (
+    <h2>Это страница профиля</h2>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-3">
+          <div className="nav flex-column nav-pills">
+            <NavLink className="nav-link" to="home">
+              Главная
+            </NavLink>
+            <NavLink className="nav-link" to="profile">
+              Профиль
+            </NavLink>
+            <NavLink className="nav-link" to="messages">
+              Сообщения
+            </NavLink>
+            <NavLink className="nav-link" to="settings">
+              Настройки
+            </NavLink>
+          </div>
+        </div>
+        <div className="col-9">
+          <Routes>
+            <Route path='/profile' element={<Profile />}/>
+          </Routes>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
