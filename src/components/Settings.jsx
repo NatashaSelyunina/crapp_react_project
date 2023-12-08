d501ce99cc7a1052860f4f5e3b5387432ff0331f
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2023-12-08 10:04:38 +0300

    fiv Settings (add image)

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,5 +1,12 @@
-
+import imagePic from ".././assets/img/coffee2.jpg";
 
 export const Settings = () => {
-  <h2>Это страница с настройками</h2>;
+  return (
+    <div className="row">
+      <div className="col-md-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias eum dolorum at nam vel fugit est ratione, minus hic non perspiciatis nemo magni culpa doloribus nulla, numquam illo enim quis.</div>
+      <div className="col-md-6" style={{backgroundImage: `url(${imagePic})`}}>
+      </div>
+      <img src={imagePic} alt="" width={200px}/>
+    </div>
+  )
 };
